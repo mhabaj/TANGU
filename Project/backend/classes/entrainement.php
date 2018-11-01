983a363c5ece81d60f50c46746129d0f57917c85
mhabaj <mhabaj99@hotmail.com>
2018-11-02 00:13:16 +0100

    AjoutBDDEntrainement
    creation class entrainement et fction ajout entrainement

--- Project/backend/classes/entrainement.php
+++ Project/backend/classes/entrainement.php
@@ -31,23 +31,37 @@ class entrainement
         $this->$nbvolees = $nbvolees;
         $this->$nbfleches = $nbfleches;
 
+
+
+        echo 'objet instancié';
+
     }
 
 
-    public function checkEnt()
-    {
+    public function checkEnt($ID_blason, $ID_arc, $nom, $lieu, $date, $distance, $nbserie, $nbvolees, $nbfleches)
+    { //on check si les valeurs saisi sont valides pour etre envoyés dans la base de données.
+
 
         if (!empty($ID_blason) and !empty($ID_arc) and !empty($nom) and !empty($lieu) and !empty($date)
             and !empty($distance) and !empty($nbserie) and !empty($nbvolees) and !empty($nbfleches)) {
 
-                if (is_numeric($ID_blason) and is_numeric($ID_arc) and sterlen($nom) <= 30 and sterlen($lieu) <= 200
-                     and verifDate($date) == true and is_numeric($distance) and $distance<=125
-                    and is_numeric($nbserie) and $nbserie<=5 and is_numeric($nbvolees) and $nbvolees<=10 and is_numeric($nbfleches) and $nbfleches<=10) {
+            if (is_numeric($ID_blason) and is_numeric($ID_arc) and strlen($nom) <= 30 and strlen($lieu) <= 200
+                /* and entrainement::verifDate($date) == true */ and is_numeric($distance) and $distance <= 125
+                and is_numeric($nbserie) and $nbserie <= 5 and is_numeric($nbvolees) and $nbvolees <= 10 and is_numeric($nbfleches) and $nbfleches <= 10) {
+                return true;
 
-
-
+            } else {
+                echo '<p>Valeurs saisis invalides!</p>';
+                return false;
 
             }
+
+
+        } else {
+
+            echo '<p>Merci de fournir toutes les informations demandées!</p>';
+            return false;
+
         }
 
 
@@ -55,8 +69,8 @@ class entrainement
 
     public function verifDate($input)
     {
-        $date = html($_POST[$input]);
-        list($d, $m, $y) = explode('-', $date);
+
+        list($d, $m, $y) = explode('-', $input);
         if (!checkdate($d, $m, $y)) {
 
             echo '<p> Date non valide</p>';
@@ -68,13 +82,25 @@ class entrainement
 
     }
 
-    public function creerEntrainement()
+    public function creerEntrainement($ID_blason, $ID_arc, $nom, $lieu, $date, $distance, $nbserie, $nbvolees, $nbfleches)
     {
+
+        if (entrainement::checkEnt($ID_blason, $ID_arc, $nom, $lieu, $date, $distance, $nbserie, $nbvolees, $nbfleches) == true) {
+
+
+           // include '../backend/includes/connexion_bdd.php';
+            include '../includes/connexion_bdd.php';
+            $requ = $bdd->exec("INSERT INTO `entrainement` (`ID_ENT`, `ID_USER`, `ID_ARC`, `ID_BLAS`, `NOM_ENT`, `LIEU_ENT`, `DATE_ENT`, `DIST_ENT`, `NBR_FLECHES`, `PTS_TOTAL`, `PCT_DIX`, `PCT_NEUF`, `MOY_ENT`, `STATUT_ENT`) VALUES (NULL, '1', '$ID_arc', '$ID_blason', '$nom', '$lieu', '2018-11-14 00:00:00', '$distance', '$nbfleches', NULL, NULL, NULL, NULL, '1')");
+
+
+            echo '<p> Entranement crée! </p>';
+
+        }
 
 
     }
 
-    public function lancerEntrainement($nbserie, $nbvolees, $nbfleches)
+     /* public function lancerEntrainement($nbserie, $nbvolees, $nbfleches)
     {
 
 
@@ -95,10 +121,24 @@ class entrainement
 
 
     }
+*/
+
+
+
+
+
 
 
 }
+$ent1=new entrainement('nom1', 'lieu1', '2018-11-14 00:00:00', 15, 1, 1, 2, 5, 3);
+$ent1->creerEntrainement(1, 1,'nom1' , 'lieu1', '2018-11-14 00:00:00', 15, 2, 5, 3);
 
 
 
 
+
+
+
+
+
+?>
