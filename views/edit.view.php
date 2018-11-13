35f3be9f224d7dc72548f646901e8cfffa360773
Prometheus <windfire23@gmail.com>
2018-11-13 13:08:43 +0100

    Nouveau Design Personnaliser

--- views/edit.view.php
+++ views/edit.view.php
@@ -10,11 +10,45 @@
                     </select>
                 </form>
             </div>
-            <div w3-include-html="arcSlides.view.php"></div>
-
+            <?php include_once 'arcSlides.view.php' ?>
             <?php include_once 'includes/footer.php';?>
         </div>
-        <script src="../assets/js/swiper.min.js"></script>
-        <script></script>
-    </body>
-</html>
+        <script src="../assets/js/swiper.js"></script>
+        <script>
+            var swiperArc = new Swiper('.swiper-container', {
+                slidesPerView: 1.3,
+                speed: 700,
+                setWrapperSize: true,
+                effect: 'slide',
+                spaceBetween: 20,
+                centeredSlides: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: false,
+                    dynamicBullets: true,
+                },
+            });
+
+            var firstSlideArc = swiperArc.slides[0];
+            firstSlideArc.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.25), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+            var nbrOfSlidesArc = swiperArc.slides.length;
+
+            for(var i = 1; i < nbrOfSlidesArc; i++) {
+                /*swiper.slides[i].style.height = "98%";*/
+                swiperArc.slides[i].style.boxShadow = "none";
+            }
+
+            swiperArc.on('slideChangeTransitionStart', function() {
+                var nbrOfSlides = swiperArc.slides.length;
+                var currentSlideId = swiperArc.activeIndex;
+
+                for (var i = 0; i < nbrOfSlides && i != currentSlideId; i++) {
+                    swiperArc.slides[i].style.transition = "box-shadow .3s ease-in-out";
+                    /*swiper.slides[i].style.height = "98%";*/
+                    swiperArc.slides[i].style.boxShadow = "none";
+                }
+                swiperArc.slides[currentSlideId].style.transition = "box-shadow .3s ease-in-out";
+                /*swiper.slides[currentSlideId].style.height = "100%";*/
+                swiperArc.slides[currentSlideId].style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+            });
+        </script>
