ec4484ebfd67eccb2c761f6ae68b4fb99da64ecb
mhabaj <mhabaj99@hotmail.com>
2018-11-21 23:24:09 +0100

    Update V5

---
+++ Versions/V5/training.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'controllers/functions/control-session.php';
+$title = "Entrainement";?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title><?= $title?></title>
+
+        <link rel="stylesheet" href="assets/css/header.css">
+        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="assets/css/navbar.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+        <script src="assets/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <?php include_once 'views/training.view.php';?>
+    </body>
+</html>
---
+++ Versions/V5/views/includes/header.php
@@ -0,0 +1,6 @@
+<div class="container-fluid header" id="headerBox">
+    <div id="titleBox">
+        <h1><?= $title?></h1>
+    </div>
+    <div id="line"></div>
+</div>
