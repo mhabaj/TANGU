d8b4f92bd4706ffdaeaaedcbac71dc6385b1f3cc
mhabaj <mhabaj99@hotmail.com>
2018-11-25 13:18:27 +0100

    2

--- Project/entrainement.php
+++ Project/entrainement.php
@@ -38,7 +38,11 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
 
                 <p> Volée:</p>
 
+<<<<<<< HEAD
                 <form method="POST" id="voleeForm" target="_myFrame" name="voleeForm" >
+=======
+                <form method="GET" id="voleeForm" target="_myFrame" name="voleeForm" action="">
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
 
                     <select name='volee' onchange='this.form.submit()'>
                         <option value="<?php echo "null"; ?>"> >>CHOISIR UNE VOLEE<<  </option>
@@ -63,6 +67,8 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
                             <option value="<?php echo $donnees['ID_VOL'] ?>">Volee numero <?php echo $n ?></option>
 
 
+
+
                             <?php
                             $n++;
                         }
@@ -71,6 +77,11 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
 
                         ?>
                     </select>
+<<<<<<< HEAD
+=======
+                    <input name="new_ID_Ent" type="hidden" value="<?php echo $new_ID_Ent ?>">
+                    <input name="Serie" type="hidden" value="<?php echo $idSerie?>">
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
                     <noscript><input type="submit" value="submit"></noscript>
                 </form>
 
@@ -78,12 +89,21 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
                 <?php
 
 
+<<<<<<< HEAD
                 if (isset($_POST['volee'])) {
 
 
                     if (!empty($_POST['volee']) And is_numeric($_POST['volee'])) {
 
                         $idVolee = $_POST['volee'];
+=======
+                if (isset($_GET['volee'])) {
+
+
+                    if (!empty($_GET['volee']) And is_numeric($_GET['volee'])) {
+
+                        $idVolee = $_GET['volee'];
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
                         include('controllers/functions/connexion_bdd.php'); //on se connect a la base et on envoie la requete
 
                         ?>
@@ -91,7 +111,11 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
 
 
 
+<<<<<<< HEAD
                 <form method="POST" id="FlecheForm" target="_myFrame" name="FlecheForm" action="" >
+=======
+                <form method="GET" id="FlecheForm" target="_myFrame" name="FlecheForm" action="entrainement.php" >
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
 <?php
                         $reponse = $bdd->query("SELECT * FROM fleche WHERE ID_VOL= '$idVolee' ORDER BY fleche.ID_FLECHE ASC");
                         $n = 1;
@@ -104,6 +128,7 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
                             <p> fleche numero<?php echo $donnees['ID_FLECHE'] ; ?></p>
                             <input name="FID<?php echo $n; ?>" type="hidden" type="number" value="<?php echo $donnees['ID_FLECHE']; ?>">
                             <input name="Score<?php echo $n; ?>" type="number"  value="">
+<<<<<<< HEAD
 
                             <?php
                             $n++;
@@ -111,11 +136,29 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
                         }
 
 
+=======
+                            <input name="new_ID_Ent" type="hidden" value="<?php echo $new_ID_Ent ?>">
+                            <input name="Serie" type="hidden" value="<?php echo $idSerie?>">
+
+                            <?php
+                            $n++;
+                            echo "je suis a l'interieur du trqitement";
+                        }
+                        ?>
+
+                        <input name="MAX" type="hidden" type="number"  value="<?php echo $n; ?>">
+
+                    <?php
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
                         $reponse->closeCursor(); // Termine le traitement de la requête
 
                         ?>
                         <input type="submit" name="MAJFleche" placeholder="Valider">
                         </form>
+<<<<<<< HEAD
+=======
+                        <p><?php echo $n; ?></p>
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
 
                         <?php
 
@@ -123,6 +166,7 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
                             $j=1;
                             while ($j<=$n){
 
+<<<<<<< HEAD
                             include('controllers/functions/connexion_bdd.php'); //on se connect a la base et on envoie la requete
 
                             $FID='FID'.$j;
@@ -135,18 +179,54 @@ $new_ID_Ent = $_GET['new_ID_Ent'];
                             $n = 1;
                             // On affiche chaque entrée une à une
 
+=======
+                        if (isset($_GET['MAJFleche'])) {
+                            echo "je suis a l'interieur du if";
+                            $j=1;
+                            $n =  $_GET['MAX'];
 
+                            while ($j<$n){
 
+                            echo "je suis a l'interieur du while";
+
+                            include('controllers/functions/connexion_bdd.php'); //on se connect a la base et on envoie la requete
+
+                            $FID='FID'.$j;
+                            $Score='Score'.$j;
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
+
+                            $FlechID=$_GET[$FID];
+                            $points=$_GET[$Score];
+
+<<<<<<< HEAD
                         }}
 
 
 
 
                         ?>
+=======
+                            $req = $bdd->exec("UPDATE `fleche` SET `PTSFLECHE` = '$points' WHERE `fleche`.`ID_FLECHE` = '$FlechID';");
+
+                            // On affiche chaque entrée une à une
+
+                            $j++;
+
+                        }}
+
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
 
 
                         <?php
 
+<<<<<<< HEAD
+=======
+                        ?>
+
+
+                        <?php
+
+>>>>>>> parent of 759402e... AJOUT FLECHES FINI
 
                     }
 
