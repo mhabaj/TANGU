d47d33a77c71e667be55b3a325d18a7e8c41e784
Matthias <20934559+MaBoCode@users.noreply.github.com>
2018-12-12 11:48:20 +0100

    Update AddTraining

--- views/add.view.php
+++ views/add.view.php
@@ -1,8 +1,11 @@
 <div class="container-fluid" id="mainBox">
-    <div id="title">
-        <p id="serieText"></p>
-        <p id="voleeText"></p>
+    <div id="titleBox">
+        <h1 id="serieText"></h1><br><br>
+        <h1 id="voleeText"></h1>
     </div>
+    <br>
+    <br>
+    <p id="tirText"></p>
     <div id="target">
         <div id="target1"></div>
         <div id="subTarget1"></div>
@@ -18,6 +21,7 @@
     </div>
 </div>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
     var target1 = document.getElementById('target1'),
         target2 = document.getElementById('target2'),
@@ -31,8 +35,10 @@
         subTarget5 = document.getElementById('subTarget5'),
         subTarget6 = document.getElementById('subTarget6');
 
+
     var serieText = document.getElementById('serieText'),
-        voleeText = document.getElementById('voleeText');
+        voleeText = document.getElementById('voleeText'),
+        tirText = document.getElementById('tirText');
 
     var nbrSeries = 2,
         nbrVolees = 3,
@@ -41,133 +47,109 @@
         countVolees = 1,
         countTirs = 0;
 
-    var checkEvents = false;
+    var nbr10 = 0,
+        nbr9 = 0,
+        nbr8 = 0;
 
-    function target1Handler() {
+    serieText.innerHTML = "Serie " + countSeries;
+    voleeText.innerHTML = "Volee " + countVolees;
+
+    function draw(x, y) {
+        let point = document.createElement('div');
+        point.className = "point";
+        point.style.left = x + "px";
+        point.style.top = y + "px";
+        document.body.appendChild(point);
+        console.log(point);
+    }
+
+    function handle(e) {
+
+        let clientX = e.touches[0].clientX;
+        let clientY = e.touches[0].clientY;
+
         if(countTirs >= nbrTirs) {
+            greetings();
             if(countVolees >= nbrVolees) {
                 if(countSeries >= nbrSeries) {
+                    $('.point').remove();
                     console.log('Fin de entrainement');
+                    killEvents();
                 } else {
                     countVolees = 0;
                     countSeries++;
                 }
             } else {
+                nbr8 = 0;
                 countTirs = 0;
                 countVolees++;
+                $('.point').remove();
             }
         } else {
-            countTirs++;
             serieText.innerHTML = "Serie " + countSeries;
             voleeText.innerHTML = "Volee " + countVolees;
+            draw(clientX, clientY);
+            countTirs++;
+            tirText.innerHTML = 'Plus que ' + (nbrTirs - countTirs) + ' tirs';
             console.log('Serie ' + countSeries + ' Volee '+ countVolees  + ' Tir ' + countTirs);
         }
     }
 
-    function target2Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            target2.removeEventListener('touchstart', target2Handler);
-        } else {
-            countTirs++;
-            console.log('target2');
-        }
+    function target1Handler(e) {
+        handle(e);
     }
 
-    function target3Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            target3.removeEventListener('touchstart', target3Handler);
-        } else {
-            countTirs++;
-            console.log('target3');
-        }
+    function target2Handler(e) {
+        handle(e);
     }
 
-    function target4Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            target4.removeEventListener('touchstart', target4Handler);
-        } else {
-            countTirs++;
-            console.log('target4');
-        }
+    function target3Handler(e) {
+        handle(e);
+        nbr8++;
     }
 
-    function target5Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            target5.removeEventListener('touchstart', target5Handler);
-        } else {
-            countTirs++;
-            console.log('target5');
-        }
+    function target4Handler(e) {
+        handle(e);
+        nbr9++;
     }
 
-    function subTarget1Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            subTarget1.removeEventListener('touchstart', subTarget1Handler);
-        } else {
-            countTirs++;
-            console.log('subtarget1');
-        }
+    function target5Handler(e) {
+        handle(e);
+        nbr10++;
     }
 
-    function subTarget2Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            subTarget2.removeEventListener('touchstart', subTarget2Handler);
-        } else {
-            countTirs++;
-            console.log('subtarget2');
-        }
+    function subTarget1Handler(e) {
+        handle(e);
     }
 
-    function subTarget3Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            subTarget3.removeEventListener('touchstart', subTarget3Handler);
-        } else {
-            countTirs++;
-            console.log('subtarget3');
-        }
+    function subTarget2Handler(e) {
+        handle(e);
     }
 
-    function subTarget4Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            subTarget4.removeEventListener('touchstart', subTarget4Handler);
-        } else {
-            countTirs++;
-            console.log('subtarget4');
-        }
+    function subTarget3Handler(e) {
+        handle(e);
+        nbr8++;
     }
 
-    function subTarget5Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            subTarget5.removeEventListener('touchstart', subTarget5Handler);
-        } else {
-            countTirs++;
-            console.log('subtarget5');
-        }
+    function subTarget4Handler(e) {
+        handle(e);
+        nbr9++;
     }
 
-    function subTarget6Handler() {
-        if(countTirs >= nbrTirs) {
-            check = true;
-            subTarget6.removeEventListener('touchstart', subTarget6Handler);
-        } else {
-            countTirs++;
-            console.log('subtarget6');
-        }
+    function subTarget5Handler(e) {
+        handle(e);
+        nbr10++;
+    }
+
+    function subTarget6Handler(e) {
+        handle(e);
+        nbr10++;
     }
 
 
     function launchEvents() {
         target1.addEventListener('touchstart', target1Handler);
-        /*target2.addEventListener('touchstart', target2Handler);
+        target2.addEventListener('touchstart', target2Handler);
         target3.addEventListener('touchstart', target3Handler);
         target4.addEventListener('touchstart', target4Handler);
         target5.addEventListener('touchstart', target5Handler);
@@ -177,8 +159,58 @@
         subTarget3.addEventListener('touchstart', subTarget3Handler);
         subTarget4.addEventListener('touchstart', subTarget4Handler);
         subTarget5.addEventListener('touchstart', subTarget5Handler);
-        subTarget6.addEventListener('touchstart', subTarget6Handler);*/
+        subTarget6.addEventListener('touchstart', subTarget6Handler);
     }
+
+    function killEvents() {
+        target1.removeEventListener('touchstart', target1Handler);
+        target2.removeEventListener('touchstart', target2Handler);
+        target3.removeEventListener('touchstart', target3Handler);
+        target4.removeEventListener('touchstart', target4Handler);
+        target5.removeEventListener('touchstart', target5Handler);
+
+        subTarget1.removeEventListener('touchstart', subTarget1Handler);
+        subTarget2.removeEventListener('touchstart', subTarget2Handler);
+        subTarget3.removeEventListener('touchstart', subTarget3Handler);
+        subTarget4.removeEventListener('touchstart', subTarget4Handler);
+        subTarget5.removeEventListener('touchstart', subTarget5Handler);
+        subTarget6.removeEventListener('touchstart', subTarget6Handler);
+    }
+
+    function pct8() {
+        return nbr8 / nbrTirs;
+    }
+
+    function pct9() {
+        return nbr9 / nbrTirs;
+    }
+
+    function pct10() {
+        return nbr10 / nbrTirs;
+    }
+
+    function greetings() {
+        var pct8 = nbr8 / nbrTirs,
+            pct9 = nbr9 / nbrTirs,
+            pct10 = nbr10 / nbrTirs;
+
+        var msg;
+
+        if(pct8 > 0.9) {
+            msg = "Pas mal!";
+        }
+
+        if(pct9 > 0.8) {
+            msg = "Bien joué !";
+        }
+
+        if(pct10 > 0.8) {
+            msg = "Wow!";
+        }
+
+        console.log(msg);
+    }
+
 
 launchEvents();
 
